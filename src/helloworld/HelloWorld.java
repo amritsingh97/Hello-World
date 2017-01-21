a99bfbddfa7b35beec44ff01cd75b0641d1bad5f
ASUS <ASUS@192.168.0.20>
2017-01-21 10:47:11 -0500

    Hello World statement

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ * Simple Java Program to print Hello World
+ *
+ * @author Amrit Singh
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World, my GitHub account name is 'amritsingh97'");
+        
+    } // end of main
+    
+} // end of class
